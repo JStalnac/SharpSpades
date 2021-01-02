0a259db31ba480848a9a2ff721417e2a29e67f7f
JStalnac <55840834+JStalnac@users.noreply.github.com>
2021-01-02 20:17:00 +0200

    Added nullables to type parameters

--- SharpSpades.Api/Utils/Throw.cs
+++ SharpSpades.Api/Utils/Throw.cs
@@ -6,19 +6,19 @@ namespace SharpSpades.Api.Utils
 {
     public static class Throw
     {
-        public static void IfNull<T>(T obj) where T : class
+        public static void IfNull<T>(T? obj) where T : class
         {
             if (obj is null)
                 throw new ArgumentNullException();
         }
 
-        public static void IfNull<T>(T obj, string? paramName) where T : class
+        public static void IfNull<T>(T? obj, string? paramName) where T : class
         {
             if (obj is null)
                 throw new ArgumentNullException(paramName);
         }
 
-        public static void IfNull<T>(T obj, string? paramName, string? message) where T : class
+        public static void IfNull<T>(T? obj, string? paramName, string? message) where T : class
         {
             if (obj is null)
                 throw new ArgumentNullException(paramName, message);
