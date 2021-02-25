42cc2df7c0be0ce9e76489e2b69910875ff64397
JStalnac <55840834+JStalnac@users.noreply.github.com>
2021-02-25 18:19:14 +0200

    Added vxl file reading We may run into issues with the colors being Int32s. I am yet to fully test if the colors will be correct

---
+++ SharpSpades.Tests/MapTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace SharpSpades.Tests
+{
+    public class MapTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
