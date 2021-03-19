d9c51469e4bae9d7a82b9a1e1ac6b548f39d6fb4
JStalnac <55840834+JStalnac@users.noreply.github.com>
2021-03-19 18:33:57 +0200

    Added HexDump utility

---
+++ SharpSpades.Api/Utils/HexDump.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace SharpSpades.Api.Utils
+{
+    public static class HexDump
+    {
+        public static string Create(ReadOnlySpan<byte> buffer, int bytesPerLine = 16)
+        {
+            StringBuilder result = new();
+            for (int offset = 0; offset < buffer.Length; offset += bytesPerLine)
+            {
+                result.Append($"{offset:X4}\t");
+
+                for (int i = 0; i < bytesPerLine; i++)
+                {
+                    if (offset + i >= buffer.Length)
+                        break;
+                    result.Append($" {buffer[offset + i]:x2}");
+                }
+
+                result.AppendLine();
+            }
+            return result.ToString();
+        }
+    }
+}
