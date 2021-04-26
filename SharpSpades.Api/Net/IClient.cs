126a16a37f284b71d9413e7dd6d7c12e886f7ced
JStalnac <joel.m.stalnacke@gmail.com>
2021-04-26 16:03:11 +0300

    Remove the unnecessary public keyword in IClient

--- SharpSpades.Api/Net/IClient.cs
+++ SharpSpades.Api/Net/IClient.cs
@@ -1,14 +1,19 @@
-﻿using SharpSpades.Api.Net.Packets;
+﻿using SharpSpades.Api.Entities;
+using SharpSpades.Api.Net.Packets;
 using System.Threading.Tasks;
 
 #nullable enable
+
+// We may want to remove this interface and just have IPlayer
+// available in the api as this doesn't really serve a purpose
+// and it adds complexity.
 
 namespace SharpSpades.Api.Net
 {
     public interface IClient
     {
         byte Id { get; }
-        public IServer Server { get; }
+        IServer Server { get; }
         IPlayer? Player { get; }
 
         ValueTask DisconnectAsync(DisconnectReason reason);
