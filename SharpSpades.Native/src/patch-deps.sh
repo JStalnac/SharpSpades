d5216686993858579a5dc2460dc79188a29b2aa0
JStalnac <55840834+JStalnac@users.noreply.github.com>
2025-12-12 19:27:18 +0200

    Fix not being able to find the native lib
    I forgot to update the .NET version patch-deps.sh so on .NET 10 the
    runtime wouldn't add the path of the native library to deps.

--- SharpSpades.Native/src/patch-deps.sh
+++ SharpSpades.Native/src/patch-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # SPDX-License-Identifier: MIT
 depsjson="$(find "$1" -name '*.deps.json')"
-patched="$(jq '.targets.".NETCoreApp,Version=v8.0"."SharpSpades.Native/1.0.0" += {"runtimeTargets": {"runtimes/linux-x64/native/libsharpspades.so": {"rid": "linux-x64", "assetType": "native"} }}' "$depsjson")"
+patched="$(jq '.targets.".NETCoreApp,Version=v10.0"."SharpSpades.Native/1.0.0" += {"runtimeTargets": {"runtimes/linux-x64/native/libsharpspades.so": {"rid": "linux-x64", "assetType": "native"} }}' "$depsjson")"
 echo -n "$patched" > "$depsjson"
